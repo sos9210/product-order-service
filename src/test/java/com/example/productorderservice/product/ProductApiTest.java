0470bf6066779986cc6992a2f4a70cd4afd3afaa
sungsanghyun <sos9215@gmail.com>
2023-05-11 23:47:54 +0900

    상품조회기능 테스트

--- src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -7,35 +7,31 @@ import io.restassured.response.Response;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 public class ProductApiTest extends ApiTest {
 
 
     @Test
     void 상품등록() {
-        final var request = 상품등록요청_생성();
+        final var request = ProductSteps.상품등록요청_생성();
 
-        final var response = 상품등록요청(request);
+        final var response = ProductSteps.상품등록요청(request);
 
         Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
-    private ExtractableResponse<Response> 상품등록요청(AddProductRequest request) {
-        return RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
+    @Test
+    void 상품조회() {
+        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+        Long productId = 1L;
+
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
                 .when()
-                .post("/products")
-                .then()
-                .log().all().extract();
-    }
+                .get("/products/{productId}", productId)
+                .then().log().all()
+                .extract();
 
-    private AddProductRequest 상품등록요청_생성() {
-        final String name = "상품명입니다";
-        final int price = 1000;
-        final DisCountPolicy disCountPolicy = DisCountPolicy.NONE;
-        return new AddProductRequest(name, price, disCountPolicy);
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        Assertions.assertThat(response.jsonPath().getString("name")).isEqualTo("상품명입니다");
     }
-
 }
