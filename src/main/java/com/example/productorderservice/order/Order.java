3643b7b992dd73f7196669eefcaf03cb5af40a75
sungsanghyun <sos9210@naver.com>
2023-11-22 23:50:01 +0900

    상품주문 JPA적용

--- src/main/java/com/example/productorderservice/order/Order.java
+++ src/main/java/com/example/productorderservice/order/Order.java
@@ -1,21 +1,31 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 class Order {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
+    @OneToOne
+    private Product product;
+
+    private int quantity;
 
     public Order(final Product product, final int quantity) {
         Assert.notNull(product, "상품은 필수입니다.");
         Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
-    }
-
-    public void assignId(Long id) {
-        this.id = id;
+        this.product = product;
+        this.quantity = quantity;
     }
 }
